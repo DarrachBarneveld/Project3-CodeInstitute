eb4f522f7dbe15a6bc85e10da9726d373e4b01a8
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-07-04 16:12:50 +0100

    added function comments and updated indenting errors, whitespace errors and recommended python formatting of pylint

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+"""Module provides calles for manipulating dates and times"""
 from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
@@ -18,29 +19,30 @@ USERS_SHEET = SPREADSHEET.get_worksheet(0)
 WORKOUT_SHEET = SPREADSHEET.get_worksheet(1)
 
 EXERCISES = ['running', 'swimming', 'cycling', 'weights', 'sports']
+CHOICE_OPTIONS = ['Enter Workout', 'View Workouts', 'Check BMI', 'Dieting Macros Calculator', 'Recommended Daily Calories']
 
 sheet_data = USERS_SHEET.get_all_values()
 df = pd.DataFrame(sheet_data[1:], columns=sheet_data[0])
 
 
-
 def select_options(current_user):
     """
-    Displays a list of options and prompts the user to select one.
+    Displays a list of options and prompts the user to select one. Selected prompt will run an assosicated function
 
-    Returns:
-        str: The selected option.
+    Parameters:
+        current_user(str): The authenticated user email
+
+    Raises:
+        ValueError: If the choice is invalid or not within the choice amount.
     """
-    options = ['Enter Workout', 'View Workouts', 'Check BMI', 'Dieting Macros Calculator', 'Recommended Daily Calories']
-
     while True:
-        for i, option in enumerate(options):
+        for i, option in enumerate(CHOICE_OPTIONS):
             print(f"{i+1}. {option}")
         choice = input("Enter the number corresponding to your choice: ")
 
         try:
             index = int(choice) - 1
-            if 0 <= index < len(options):
+            if 0 <= index < len(CHOICE_OPTIONS):
                 if index == 0:
                     create_new_workout(current_user)
                 elif index == 1:
@@ -49,42 +51,41 @@ def select_options(current_user):
                     response = fitness_calculator.bmi_calculator()
                     print(response)
                 elif index == 3:
-                   response = fitness_calculator.dieting_macros()
-                   print(response)
+                    response = fitness_calculator.dieting_macros()
+                    print(response)
                 elif index == 4:
-                   response =  fitness_calculator.daily_calories()
-                   print(response)
+                    response = fitness_calculator.daily_calories()
+                    print(response)
             else:
                 print("Invalid choice. Please enter a valid number.")
         except ValueError:
             print("Invalid choice. Please enter a valid number.")
 
-    
-    
 
 def view_all_workouts(current_user):
-        """
+    """
     Retrieves data from a Google Sheets spreadsheet.
 
     Parameters:
-        sheet_id (str): The ID of the Google Sheets spreadsheet.
-        sheet_name (str): The name of the sheet within the spreadsheet.
-
-    Returns:
-        list: A 2D list containing the data from the specified sheet.
+        current_user(str): The authenticated user email
     """
-        all_workouts = WORKOUT_SHEET.get_all_values()
+    all_workouts = WORKOUT_SHEET.get_all_values()
 
-        filtered_data = [row for row in all_workouts if row[0] == current_user]
+    filtered_data = [row for row in all_workouts if row[0] == current_user]
 
-        for row in filtered_data:
-            workout_type, workout_time, workout_duration = row[1:4]
-            print("Type:", workout_type, "Time:", workout_time, "Duration:", workout_duration )
-            print()  # Print an empty line between rows
-
+    for row in filtered_data:
+        workout_type, workout_time, workout_duration = row[1:4]
+        print("Type:", workout_type, "Time:", workout_time, "Duration:", workout_duration )
+        print()  # Print an empty line between rows
 
 
 def create_new_workout(current_user):
+    """
+    Create a new workout in google sheets document with user inputs and authenticated user so workouts are saved with user data
+    
+    Args:
+        current_user(str): The authenticated user email
+    """
     workout_type = input('What workout type did you do?')
     workout_duration = input('For how long did you workout in whole minutes?')
     validate_data(workout_type, workout_duration)
@@ -92,48 +93,43 @@ def create_new_workout(current_user):
     update_workout_sheet(current_user,workout_type, workout_duration)
 
 
-
-
-def validate_data(type, duration):
+def validate_data(workout_type, duration):
     """
     Check if a inputed string is a valid exercise. Check if the duration is a number
     
     Args:
-        string (str): The string to search for.
-        EXERCISES (list): The array (list) to search in.
+        workout_type (str): The type of workout to search for.
+        duration (str): The amount of time a user worked out for
     
     Raises:
         ValueError: If the search string is not found in the array.
+        ValueError: If the duration is not a number.
     """
     try:
-        if type not in EXERCISES:
-            raise ValueError(f"{type} does not exist in the array")
-        else:
-            print(type, "exists in the array")
+        if workout_type not in EXERCISES:
+            raise ValueError(f"{workout_type} does not exist in the array")
         duration = int(duration)
-        if 0 <= duration <= 240:
-            return True  # Valid integer within the specified range
-        else:
+        if not 0 <= duration <= 240:
             raise ValueError(f"{duration} is not a number")
+             
     except ValueError as error:
         print("Error:", str(error))
-     
     return True
 
-def update_workout_sheet(current_user, type, duration):
+
+def update_workout_sheet(current_user, workout_type, duration):
     """
     Update the Google Sheets Document if valid data
     
     Args:
-        The current data within the Google Sheets Document
-    
-    Raises:
-        ValueError: If the search string is not found in the array.
+        current_user (str): The authenticated user email
+        type (str): The type of workout
+        duration (str): The duration of the workout
     """
     current_time = datetime.now().time()
     time_string = current_time.strftime("%H:%M:%S")
 
-    workout_row = [current_user, type, time_string, duration]
+    workout_row = [current_user, workout_type, time_string, duration]
     WORKOUT_SHEET.append_row(workout_row)
 
 
@@ -145,13 +141,11 @@ def main():
     current_user = ''
 
     if choice == 'login':
-       current_user = login(df)
+        current_user = login(df)
     elif choice == 'signup':
         current_user = signup(USERS_SHEET)
     else:
-        print("Invalid choice")
-
-    
+        print("Invalid choice")   
     if current_user:
         select_options(current_user)
     else:
