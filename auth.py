aa0018178d257de75e5f54e0fe023a9bd7f770eb
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-07-06 15:58:28 +0100

    added docstring comments to auth.py for module and all functions

--- auth.py
+++ auth.py
@@ -1,4 +1,16 @@
+"""This module provides functions for user authentication and authorization"""
+
 def login(dataframe):
+    """
+    Login function that verifies the username and name in a Pandas DataFrame.
+
+    Args:
+        dataframe (DataFrame): The DataFrame containing user data.
+
+    Returns:
+        str or None: The email address if the login is successful, or None if the login fails.
+
+    """
     first_name = input("Enter your first name: ").lower()
     last_name = input("Enter your last name: ").lower()
     email = input("Enter your email: ").lower()
@@ -6,9 +18,8 @@ def login(dataframe):
     dataframe['Firstname'] = dataframe['Firstname'].str.lower()
     dataframe['Lastname'] = dataframe['Lastname'].str.lower()
     dataframe['Email'] = dataframe['Email'].str.lower()
-
+    # pylint: disable=line-too-long
     matched_users = dataframe[(dataframe['Firstname'] == first_name) & (dataframe['Lastname'] == last_name) & (dataframe['Email'] == email)]
-
     # Authenticate the user based on the match
     if len(matched_users) > 0:
         print("Authentication successful")
@@ -19,7 +30,18 @@ def login(dataframe):
 
 
 def signup(sheet):
-    # Prompt the user for sign-up details
+    """
+    Signup function that appends a user's data as a row to a Google Sheet.
+
+    Args:
+        sheet (object): The Google Sheet to append the row.
+
+    Returns:
+        str: A current user email.
+
+    Raises:
+        Exception: For any other unknown errors that may occur during the signup process.
+    """
     first_name = input("Enter your first name: ")
     last_name = input("Enter your last name: ")
     email = input("Enter your email: ")
@@ -27,8 +49,7 @@ def signup(sheet):
     try:
         sheet.append_row([first_name, last_name, email])
         print("Sign-up successful")
-        return email
-    # pylint: disable=pylint(broad-exception-caught)
+        return email    # pylint: disable=pylint(broad-exception-caught)
     except Exception as exc:
         # pylint: disable=pylint(broad-exception-raised)
         raise Exception("There was an error signing up. Please try again!") from exc
